7adfebea64ccb4c2b6143278c3c333910c069700
iosh <454053152@qq.com>
2024-05-26 19:46:50 +0800

    type: update address type

--- src/accounts/utils/parseAccount.ts
+++ src/accounts/utils/parseAccount.ts
@@ -1,11 +1,10 @@
-import type { Address } from 'abitype'
+import type { ErrorType } from "../../errors/utils.js";
+import type { Account, Address } from "../types.js";
 
-import type { ErrorType } from '../../errors/utils.js'
-import type { Account } from '../types.js'
-
-export type ParseAccountErrorType = ErrorType
+export type ParseAccountErrorType = ErrorType;
 
 export function parseAccount(account: Address | Account): Account {
-  if (typeof account === 'string') return { address: account, type: 'json-rpc' }
-  return account
+  if (typeof account === "string")
+    return { address: account, type: "json-rpc" };
+  return account;
 }
