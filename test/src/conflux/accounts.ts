1136e8d4c100254240c4621e9f5dd46ec29aa6a7
iosh <454053152@qq.com>
2024-06-20 10:30:33 +0800

    chore: add test accounts

---
+++ test/src/conflux/accounts.ts
@@ -0,0 +1,44 @@
+export const accounts = [
+  {
+    privateKey:
+      "34fce6e29a7b0dbd92f51a3d0a3d4db83f55958ff6552e6f0d4ee3ef21cd2d3e",
+    hexAddress: "0x156f6c9df2f05f1e9c15b10dbebd8851c9b4842f",
+  },
+  {
+    privateKey:
+      "d6a7c99b9036ab9fda511a49e8401783f00960103622659b381dd0174f63880c",
+    hexAddress: "0x153ebbc6bfceeb54d8eff324c781290a952cd60c",
+  },
+  {
+    privateKey: '306c52752a5b6169606f20e61d1c7c192cfb7342d55b2b737c639309918c6a59',
+    hexAddress: '0x1d9dd99dbdeb9aaff5a0b7d7b4fa38d9ef11a38d'
+  },
+  {
+    privateKey: 'aac8c722ef208be93da651876c0df21b7f837d65e381ca335cab15e069ce8861',
+    hexAddress: '0x1546e522e8f48f88f83fdffe0aaaa341ba289c5a'
+  },
+  {
+    privateKey: '6e87d48346aeae3456037263ac2df038339ee0a8493cc007cb9f3c0d3f3aadab',
+    hexAddress: '0x15ba3b320d1376e6fee6a0353218075a89ab44be'
+  },
+  {
+    privateKey: '64ffb5a4a0dba5bc8a2783e701c7466a80e2e266613b63fd47dc6bf03f89581b',
+    hexAddress: '0x1f91f4668ff7f9851e8690739f617df494e0db22'
+  },
+  {
+    privateKey: 'dec3911f8beaa9995f61b0a41f6834bc7f3da62f1da6d09c49bdb34576644d8d',
+    hexAddress: '0x1b0f6a614f8b98d4b15eeef3daaee7da36a9213d'
+  },
+  {
+    privateKey: '6624e0de6a6407809b1f91fa21fd673d5664c61b581114a4f9600ed6d2327f46',
+    hexAddress: '0x1feae75b3ec40a2877a40770c0771474f176e7de'
+  },
+  {
+    privateKey: '041816c55f0f6e295ee9faf56335db23bbce65274de7c7f715c28da2be485030',
+    hexAddress: '0x119e8ff9d4784aac9ac614a074ecf77cdc9ac340'
+  },
+  {
+    privateKey: 'f4e88fd5c0fda9f96df2c238190e0869e6a520cb0ac51c5eaf3814828004707a',
+    hexAddress: '0x1d3b4acd9d291f91c0abab8e8b1f0751043de8d2'
+  }
+];
