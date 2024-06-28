ebcaa2c3450cd544bea4e1ffa062ea710a1638db
iosh <454053152@qq.com>
2024-06-28 22:21:32 +0800

    chore: add formatGDrip test

---
+++ src/unit/formatGDrip.test.ts
@@ -0,0 +1,37 @@
+import { expect, test } from 'vitest'
+
+import { formatGDrip } from './formatGDrip.js'
+import { Drip } from 'js-conflux-sdk'
+
+test('converts wei to gwei', () => {
+  expect(formatGDrip(82079252892807n)).toBe(new Drip(82079252892807n).toGDrip())
+
+  expect(formatGDrip(82079252892807n)).toMatchInlineSnapshot(`"82079.252892807"`)
+  expect(formatGDrip(82070000000000n)).toMatchInlineSnapshot(`"82070"`)
+  expect(formatGDrip(1000000000n)).toMatchInlineSnapshot('"1"')
+  expect(formatGDrip(500000000n)).toMatchInlineSnapshot('"0.5"')
+  expect(formatGDrip(100000000n)).toMatchInlineSnapshot('"0.1"')
+  expect(formatGDrip(10000000n)).toMatchInlineSnapshot('"0.01"')
+  expect(formatGDrip(1000000n)).toMatchInlineSnapshot('"0.001"')
+  expect(formatGDrip(100000n)).toMatchInlineSnapshot('"0.0001"')
+  expect(formatGDrip(10000n)).toMatchInlineSnapshot('"0.00001"')
+  expect(formatGDrip(1000n)).toMatchInlineSnapshot('"0.000001"')
+  expect(formatGDrip(100n)).toMatchInlineSnapshot('"0.0000001"')
+  expect(formatGDrip(10n)).toMatchInlineSnapshot('"0.00000001"')
+  expect(formatGDrip(1n)).toMatchInlineSnapshot('"0.000000001"')
+  expect(formatGDrip(-82079252892807n)).toMatchInlineSnapshot(
+    `"-82079.252892807"`,
+  )
+  expect(formatGDrip(-82070000000000n)).toMatchInlineSnapshot(`"-82070"`)
+  expect(formatGDrip(-1000000000n)).toMatchInlineSnapshot('"-1"')
+  expect(formatGDrip(-500000000n)).toMatchInlineSnapshot('"-0.5"')
+  expect(formatGDrip(-100000000n)).toMatchInlineSnapshot('"-0.1"')
+  expect(formatGDrip(-10000000n)).toMatchInlineSnapshot('"-0.01"')
+  expect(formatGDrip(-1000000n)).toMatchInlineSnapshot('"-0.001"')
+  expect(formatGDrip(-100000n)).toMatchInlineSnapshot('"-0.0001"')
+  expect(formatGDrip(-10000n)).toMatchInlineSnapshot('"-0.00001"')
+  expect(formatGDrip(-1000n)).toMatchInlineSnapshot('"-0.000001"')
+  expect(formatGDrip(-100n)).toMatchInlineSnapshot('"-0.0000001"')
+  expect(formatGDrip(-10n)).toMatchInlineSnapshot('"-0.00000001"')
+  expect(formatGDrip(-1n)).toMatchInlineSnapshot('"-0.000000001"')
+})
