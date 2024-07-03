2b42a360938434839a9c099fe06fe05c95fe5141
iosh <454053152@qq.com>
2024-07-03 21:34:12 +0800

    chore: update getBalance test

--- src/actions/public/getBalance.test.ts
+++ src/actions/public/getBalance.test.ts
@@ -5,6 +5,12 @@ import { privateKeyToAccount } from '../../accounts/privateKeyToAccount.js'
 import { sayHelloLocalNode } from '../localNode/sayHelloLocalNode.js'
 import { getBalance } from './getBalance.js'
 import { getBlock } from './getBlock.js'
+import { prepareTransactionRequest } from '../wallet/prepareTransactionRequest.js'
+
+import { parseCFX } from '../../unit/parseCFX.js'
+import { signTransaction } from '../../accounts/index.js'
+import { generateLocalNodeBlock } from '../localNode/generateLocalNodeBlock.js'
+import { sendRawTransaction } from '../wallet/sendRawTransaction.js'
 
 const sourceAccount = getTestAccount(accounts[0])
 const targetAccount = getTestAccount(accounts[1])
@@ -43,4 +49,27 @@ test('gets balance at block number', async () => {
       epochNumber: currentBlock.blockNumber,
     }),
   ).toMatchInlineSnapshot('10000000000000000000000n')
+})
+
+test('gets balance when transaction', async () => {
+  const tx = await prepareTransactionRequest(client, {
+    value: parseCFX('10'),
+    to: targetAccount.address,
+    account: sourceAccount,
+  })
+  const serializedTransaction = await signTransaction({
+    privateKey: accounts[0].privateKey,
+    transaction: tx,
+  })
+
+  await sendRawTransaction(client, { serializedTransaction })
+  await generateLocalNodeBlock(client, { numTxs: 1, blockSizeLimit: 1024 })
+
+  expect(
+    await getBalance(client, { address: sourceAccount.address }),
+  ).toMatchInlineSnapshot('9989999974800000000000n')
+
+  expect(
+    await getBalance(client, { address: targetAccount.address }),
+  ).toMatchInlineSnapshot('10010000000000000000000n')
 })
