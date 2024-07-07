f7c25e97781e1dede4bfc1a1e09e47f72af8161d
iosh <454053152@qq.com>
2024-07-07 16:03:40 +0800

    chore: add getDepositList test

---
+++ src/actions/public/getDepositList.test.ts
@@ -0,0 +1,79 @@
+import { afterAll, beforeAll, expect, test } from 'vitest'
+import { devConflux } from '~test/src/conflux/client.js'
+import { sayHelloLocalNode } from '../localNode/sayHelloLocalNode.js'
+import { getDepositList } from './getDepositList.js'
+import { accounts } from '~test/src/constants.js'
+import { generateEmptyLocalNodeBlocks } from '../localNode/generateEmptyLocalNodeBlocks.js'
+
+const client = devConflux.getClient()
+beforeAll(async () => {
+  await devConflux.start()
+  await sayHelloLocalNode(client)
+})
+
+afterAll(async () => {
+  await devConflux.stop()
+})
+
+test('default', async () => {
+  expect(
+    await getDepositList(client, { address: accounts[0].base32Address }),
+  ).toMatchInlineSnapshot('[]')
+})
+
+test('with args epoch tag', async () => {
+  await generateEmptyLocalNodeBlocks(client, { numBlocks: 10 })
+  expect(
+    await getDepositList(client, {
+      address: accounts[0].base32Address,
+      epochTag: 'earliest',
+    }),
+  ).toMatchInlineSnapshot('[]')
+  expect(
+    await getDepositList(client, {
+      address: accounts[0].base32Address,
+      epochTag: 'latest_checkpoint',
+    }),
+  ).toMatchInlineSnapshot('[]')
+  expect(
+    await getDepositList(client, {
+      address: accounts[0].base32Address,
+      epochTag: 'latest_confirmed',
+    }),
+  ).toMatchInlineSnapshot('[]')
+  expect(
+    await getDepositList(client, {
+      address: accounts[0].base32Address,
+      epochTag: 'latest_finalized',
+    }),
+  ).toMatchInlineSnapshot('[]')
+  expect(
+    await getDepositList(client, {
+      address: accounts[0].base32Address,
+      epochTag: 'latest_state',
+    }),
+  ).toMatchInlineSnapshot('[]')
+})
+
+test('with args epoch number', async () => {
+  await generateEmptyLocalNodeBlocks(client, { numBlocks: 50 })
+
+  expect(
+    await getDepositList(client, {
+      address: accounts[0].base32Address,
+      epochNumber: 10n,
+    }),
+  ).toMatchInlineSnapshot('[]')
+  expect(
+    await getDepositList(client, {
+      address: accounts[0].base32Address,
+      epochNumber: 20n,
+    }),
+  ).toMatchInlineSnapshot('[]')
+  expect(
+    await getDepositList(client, {
+      address: accounts[0].base32Address,
+      epochNumber: 30n,
+    }),
+  ).toMatchInlineSnapshot('[]')
+})
