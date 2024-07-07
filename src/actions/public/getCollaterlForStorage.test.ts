7bb6e8a4dbc1722862500a93c06e889e78cdb3b0
iosh <454053152@qq.com>
2024-07-07 15:08:54 +0800

    chore: add getCollateralForStorage test

---
+++ src/actions/public/getCollaterlForStorage.test.ts
@@ -0,0 +1,86 @@
+import { afterAll, beforeAll, expect, test } from 'vitest'
+import { devConflux } from '~test/src/conflux/client.js'
+import { accounts } from '~test/src/constants.js'
+import { generateEmptyLocalNodeBlocks } from '../localNode/generateEmptyLocalNodeBlocks.js'
+import { sayHelloLocalNode } from '../localNode/sayHelloLocalNode.js'
+import { getCollateralForStorage } from './getCollaterlForStorage.js'
+
+const client = devConflux.getClient()
+beforeAll(async () => {
+  await devConflux.start()
+  await sayHelloLocalNode(client)
+})
+
+afterAll(async () => {
+  await devConflux.stop()
+})
+
+test('default', async () => {
+  expect(
+    await getCollateralForStorage(client, {
+      address: accounts[0].base32Address,
+    }),
+  ).toMatchInlineSnapshot('0n')
+})
+
+test('with args epoch tag', async () => {
+  expect(
+    await getCollateralForStorage(client, {
+      address: accounts[0].base32Address,
+      epochTag: 'earliest',
+    }),
+  ).toMatchInlineSnapshot('0n')
+
+  expect(
+    await getCollateralForStorage(client, {
+      address: accounts[0].base32Address,
+      epochTag: 'latest_checkpoint',
+    }),
+  ).toMatchInlineSnapshot('0n')
+
+  expect(
+    await getCollateralForStorage(client, {
+      address: accounts[0].base32Address,
+      epochTag: 'latest_confirmed',
+    }),
+  ).toMatchInlineSnapshot('0n')
+
+  expect(
+    await getCollateralForStorage(client, {
+      address: accounts[0].base32Address,
+      epochTag: 'latest_finalized',
+    }),
+  ).toMatchInlineSnapshot('0n')
+
+  expect(
+    await getCollateralForStorage(client, {
+      address: accounts[0].base32Address,
+      epochTag: 'latest_state',
+    }),
+  ).toMatchInlineSnapshot('0n')
+})
+
+test('with args epoch number', async () => {
+  await generateEmptyLocalNodeBlocks(client, { numBlocks: 50 })
+
+  expect(
+    await getCollateralForStorage(client, {
+      address: accounts[0].base32Address,
+      epochNumber: 10n,
+    }),
+  ).toMatchInlineSnapshot('0n')
+
+  expect(
+    await getCollateralForStorage(client, {
+      address: accounts[0].base32Address,
+      epochNumber: 20n,
+    }),
+  ).toMatchInlineSnapshot('0n')
+
+  expect(
+    await getCollateralForStorage(client, {
+      address: accounts[0].base32Address,
+      epochNumber: 30n,
+    }),
+  ).toMatchInlineSnapshot('0n')
+})
