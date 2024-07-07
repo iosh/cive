9ae43227e04ccabdd115fb34ff0a381d77ad4a02
iosh <454053152@qq.com>
2024-07-07 20:36:48 +0800

    chore: add simple getFilterChanges test

---
+++ src/actions/public/getFilterChanges.test.ts
@@ -0,0 +1,44 @@
+import { afterAll, assertType, beforeAll, expect, test } from 'vitest'
+import { devConflux } from '~test/src/conflux/client.js'
+import { accounts, getTestAccount } from '~test/src/constants.js'
+import type { Hash } from '../../types/misc.js'
+import { parseCFX } from '../../unit/parseCFX.js'
+import { generateEmptyLocalNodeBlocks } from '../localNode/generateEmptyLocalNodeBlocks.js'
+import { generateLocalNodeBlock } from '../localNode/generateLocalNodeBlock.js'
+import { sayHelloLocalNode } from '../localNode/sayHelloLocalNode.js'
+import { sendTransaction } from '../wallet/sendTransaction.js'
+import { createBlockFilter } from './createBlockFilter.js'
+import { createPendingTransactionFilter } from './createPendingTransactionFilter.js'
+import { getFilterChanges } from './getFilterChanges.js'
+
+const client = devConflux.getClient()
+beforeAll(async () => {
+  await devConflux.start()
+  await sayHelloLocalNode(client)
+})
+
+afterAll(async () => {
+  await devConflux.stop()
+})
+
+test('default', async () => {
+  const filter = await createPendingTransactionFilter(client)
+  expect(await getFilterChanges(client, { filter })).toMatchInlineSnapshot('[]')
+})
+
+test('new blocks', async () => {
+  const filter = await createBlockFilter(client)
+  await generateEmptyLocalNodeBlocks(client, { numBlocks: 6 })
+  let hashes = await getFilterChanges(client, { filter })
+
+  assertType<Hash[]>(hashes)
+  expect(hashes.length).toBe(2)
+
+  hashes = await getFilterChanges(client, { filter })
+  expect(hashes.length).toBe(0)
+
+  await generateEmptyLocalNodeBlocks(client, { numBlocks: 1 })
+
+  hashes = await getFilterChanges(client, { filter })
+  expect(hashes.length).toBe(1)
+})
