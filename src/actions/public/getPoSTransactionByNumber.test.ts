f79e9ea57f5388498bece1828224725ef542b8c7
iosh <454053152@qq.com>
2024-07-09 21:26:59 +0800

    chore: add getPoSTransactionByNumber test

---
+++ src/actions/public/getPoSTransactionByNumber.test.ts
@@ -0,0 +1,20 @@
+import { afterAll, beforeAll, expect, test } from 'vitest'
+import { devConflux } from '~test/src/conflux/client.js'
+import { sayHelloLocalNode } from '../localNode/sayHelloLocalNode.js'
+import { getPoSTransactionByNumber } from './getPoSTransactionByNumber.js'
+
+const client = devConflux.getClient()
+beforeAll(async () => {
+  await devConflux.start()
+  await sayHelloLocalNode(client)
+})
+
+afterAll(async () => {
+  await devConflux.stop()
+})
+
+test('default', async () => {
+  expect(
+    await getPoSTransactionByNumber(client, { transactionNumber: 0n }),
+  ).toBeDefined()
+})
