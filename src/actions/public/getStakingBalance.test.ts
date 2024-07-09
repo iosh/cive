f1165e8d534cf1459e5d9318f1469275edd8c8ff
iosh <454053152@qq.com>
2024-07-09 21:58:49 +0800

    chore: add getStakingBalance test

---
+++ src/actions/public/getStakingBalance.test.ts
@@ -0,0 +1,72 @@
+import { afterAll, beforeAll, expect, test } from 'vitest'
+import { devConflux } from '~test/src/conflux/client.js'
+import { sayHelloLocalNode } from '../localNode/sayHelloLocalNode.js'
+import { getStakingBalance } from './getStakingBalance.js'
+import { accounts } from '~test/src/constants.js'
+import { generateEmptyLocalNodeBlocks } from '../localNode/generateEmptyLocalNodeBlocks.js'
+
+const client = devConflux.getClient()
+beforeAll(async () => {
+  await devConflux.start()
+  await sayHelloLocalNode(client)
+})
+
+afterAll(async () => {
+  await devConflux.stop()
+})
+
+test('default', async () => {
+  await generateEmptyLocalNodeBlocks(client, { numBlocks: 10 })
+  expect(
+    await getStakingBalance(client, { address: accounts[0].base32Address }),
+  ).toMatchInlineSnapshot('0n')
+})
+
+test('epochNumber', async () => {
+  expect(
+    await getStakingBalance(client, {
+      address: accounts[0].base32Address,
+      epochNumber: 1n,
+    }),
+  ).toMatchInlineSnapshot('0n')
+  expect(
+    await getStakingBalance(client, {
+      address: accounts[0].base32Address,
+      epochNumber: 3n,
+    }),
+  ).toMatchInlineSnapshot('0n')
+
+  expect(
+    await getStakingBalance(client, {
+      address: accounts[0].base32Address,
+      epochNumber: 6n,
+    }),
+  ).toMatchInlineSnapshot('0n')
+})
+
+test('epoch tag', async () => {
+  expect(
+    await getStakingBalance(client, {
+      address: accounts[0].base32Address,
+      epochTag: 'latest_state',
+    }),
+  ).toMatchInlineSnapshot('0n')
+  expect(
+    await getStakingBalance(client, {
+      address: accounts[0].base32Address,
+      epochTag: 'latest_confirmed',
+    }),
+  ).toMatchInlineSnapshot('0n')
+  expect(
+    await getStakingBalance(client, {
+      address: accounts[0].base32Address,
+      epochTag: 'latest_checkpoint',
+    }),
+  ).toMatchInlineSnapshot('0n')
+  expect(
+    await getStakingBalance(client, {
+      address: accounts[0].base32Address,
+      epochTag: 'earliest',
+    }),
+  ).toMatchInlineSnapshot('0n')
+})
