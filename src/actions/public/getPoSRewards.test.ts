b107eef709b82883504812ca51bdee768d499aee
iosh <454053152@qq.com>
2024-07-09 21:19:18 +0800

    chore: add getPoSRewards test

---
+++ src/actions/public/getPoSRewards.test.ts
@@ -0,0 +1,18 @@
+import { afterAll, beforeAll, expect, test } from 'vitest'
+import { devConflux } from '~test/src/conflux/client.js'
+import { sayHelloLocalNode } from '../localNode/sayHelloLocalNode.js'
+import { getPoSRewards } from './getPoSRewards.js'
+
+const client = devConflux.getClient()
+beforeAll(async () => {
+  await devConflux.start()
+  await sayHelloLocalNode(client)
+})
+
+afterAll(async () => {
+  await devConflux.stop()
+})
+
+test('default', async () => {
+  expect(await getPoSRewards(client, { epochNumber: 0n })).toBeNull()
+})
