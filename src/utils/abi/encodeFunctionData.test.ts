ab9bbc9bfe2bc3bf263ea75d235ff38a4c35b18f
iosh <454053152@qq.com>
2024-07-17 21:50:04 +0800

    chore: add encodeFunctionData and test

---
+++ src/utils/abi/encodeFunctionData.test.ts
@@ -0,0 +1,161 @@
+import { expect, test } from 'vitest'
+
+import { erc20Abi } from 'abitype/abis'
+import { encodeFunctionData } from './encodeFunctionData.js'
+import { prepareEncodeFunctionData } from './prepareEncodeFunctionData.js'
+import { accounts } from '../../../test/src/constants.js'
+test('foo()', () => {
+  expect(
+    encodeFunctionData({
+      abi: [
+        {
+          inputs: [],
+          name: 'foo',
+          outputs: [],
+          stateMutability: 'nonpayable',
+          type: 'function',
+        },
+      ],
+      functionName: 'foo',
+    }),
+  ).toEqual('0xc2985578')
+  expect(
+    encodeFunctionData({
+      abi: [
+        {
+          name: 'foo',
+          outputs: [],
+          stateMutability: 'nonpayable',
+          type: 'function',
+        },
+      ],
+      functionName: 'foo',
+    }),
+  ).toEqual('0xc2985578')
+})
+
+test('bar(uint256)', () => {
+  expect(
+    encodeFunctionData({
+      abi: [
+        {
+          inputs: [
+            {
+              name: 'a',
+              type: 'uint256',
+            },
+          ],
+          name: 'bar',
+          outputs: [],
+          stateMutability: 'nonpayable',
+          type: 'function',
+        },
+      ],
+      functionName: 'bar',
+      args: [1n],
+    }),
+  ).toEqual(
+    '0x0423a1320000000000000000000000000000000000000000000000000000000000000001',
+  )
+})
+
+test('getVoter((uint256,bool,address,uint256))', () => {
+  expect(
+    encodeFunctionData({
+      abi: [
+        {
+          inputs: [
+            {
+              components: [
+                {
+                  name: 'weight',
+                  type: 'uint256',
+                },
+                {
+                  name: 'voted',
+                  type: 'bool',
+                },
+                {
+                  name: 'delegate',
+                  type: 'address',
+                },
+                {
+                  name: 'vote',
+                  type: 'uint256',
+                },
+              ],
+              name: 'voter',
+              type: 'tuple',
+            },
+          ],
+          name: 'getVoter',
+          outputs: [],
+          stateMutability: 'nonpayable',
+          type: 'function',
+        },
+      ],
+      functionName: 'getVoter',
+      args: [
+        {
+          delegate: accounts[0].base32Address,
+          vote: 41n,
+          voted: true,
+          weight: 69420n,
+        },
+      ],
+    }),
+  ).toEqual(
+    '0xf37414670000000000000000000000000000000000000000000000000000000000010f2c0000000000000000000000000000000000000000000000000000000000000001000000000000000000000000156f6c9df2f05f1e9c15b10dbebd8851c9b4842f0000000000000000000000000000000000000000000000000000000000000029',
+  )
+})
+test('inferred functionName', () => {
+  expect(
+    encodeFunctionData({
+      abi: [
+        {
+          inputs: [
+            {
+              name: 'a',
+              type: 'uint256',
+            },
+          ],
+          name: 'bar',
+          outputs: [],
+          stateMutability: 'nonpayable',
+          type: 'function',
+        },
+      ],
+      args: [1n],
+    }),
+  ).toEqual(
+    '0x0423a1320000000000000000000000000000000000000000000000000000000000000001',
+  )
+})
+
+test('selector as functionName', () => {
+  const data = encodeFunctionData({
+    abi: erc20Abi,
+    functionName: '0xa9059cbb',
+    args: [accounts[0].base32Address, 69420n],
+  })
+  expect(data).toMatchInlineSnapshot(
+    `"0xa9059cbb000000000000000000000000156f6c9df2f05f1e9c15b10dbebd8851c9b4842f0000000000000000000000000000000000000000000000000000000000010f2c"`,
+  )
+})
+
+test('prepared', () => {
+  const transfer = prepareEncodeFunctionData({
+    abi: erc20Abi,
+    functionName: 'transfer',
+  })
+  const data_prepared = encodeFunctionData({
+    ...transfer,
+    args: [accounts[0].base32Address, 69420n],
+  })
+  const data = encodeFunctionData({
+    abi: erc20Abi,
+    functionName: 'transfer',
+    args: [accounts[0].base32Address, 69420n],
+  })
+  expect(data_prepared).toEqual(data)
+})
---
+++ src/utils/abi/encodeFunctionData.ts
@@ -0,0 +1,95 @@
+import type { Abi, AbiStateMutability, ExtractAbiFunctions } from '../../types/abitype.js'
+import type { ErrorType } from '../../errors/utils.js'
+import type { Hex } from '../../types/misc.js'
+import type { IsNarrowable, UnionEvaluate } from '../../types/utils.js'
+import {
+  type EncodeAbiParametersErrorType,
+  encodeAbiParameters,
+} from './encodeAbiParameters.js'
+import {
+  concatHex,
+  type AbiFunctionNotFoundErrorType,
+  type ConcatHexErrorType,
+  type ContractFunctionName,
+  type GetAbiItemErrorType,
+  type ToFunctionSelectorErrorType,
+} from 'viem'
+import type { ContractFunctionArgs } from '../../types/contract.js'
+import type { FormatAbiItemErrorType } from 'viem/utils'
+import { prepareEncodeFunctionData } from './prepareEncodeFunctionData.js'
+
+export type EncodeFunctionDataParameters<
+  abi extends Abi | readonly unknown[] = Abi,
+  functionName extends
+    | ContractFunctionName<abi>
+    | Hex
+    | undefined = ContractFunctionName<abi>,
+  ///
+  hasFunctions = abi extends Abi
+    ? Abi extends abi
+      ? true
+      : [ExtractAbiFunctions<abi>] extends [never]
+        ? false
+        : true
+    : true,
+  allArgs = ContractFunctionArgs<
+    abi,
+    AbiStateMutability,
+    functionName extends ContractFunctionName<abi>
+      ? functionName
+      : ContractFunctionName<abi>
+  >,
+  allFunctionNames = ContractFunctionName<abi>,
+> = {
+  abi: abi
+} & UnionEvaluate<
+  IsNarrowable<abi, Abi> extends true
+    ? abi['length'] extends 1
+      ? { functionName?: functionName | allFunctionNames | Hex | undefined }
+      : { functionName: functionName | allFunctionNames | Hex }
+    : { functionName?: functionName | allFunctionNames | Hex | undefined }
+> &
+  UnionEvaluate<
+    readonly [] extends allArgs
+      ? { args?: allArgs | undefined }
+      : { args: allArgs }
+  > &
+  (hasFunctions extends true ? unknown : never)
+
+export type EncodeFunctionDataReturnType = Hex
+
+export type EncodeFunctionDataErrorType =
+  | AbiFunctionNotFoundErrorType
+  | ConcatHexErrorType
+  | EncodeAbiParametersErrorType
+  | FormatAbiItemErrorType
+  | GetAbiItemErrorType
+  | ToFunctionSelectorErrorType
+  | ErrorType
+
+export function encodeFunctionData<
+  const abi extends Abi | readonly unknown[],
+  functionName extends ContractFunctionName<abi> | undefined = undefined,
+>(
+  parameters: EncodeFunctionDataParameters<abi, functionName>,
+): EncodeFunctionDataReturnType {
+  const { args } = parameters as EncodeFunctionDataParameters
+
+  const { abi, functionName } = (() => {
+    if (
+      parameters.abi.length === 1 &&
+      parameters.functionName?.startsWith('0x')
+    )
+      return parameters as { abi: Abi; functionName: Hex }
+    return prepareEncodeFunctionData(parameters)
+  })()
+
+  const abiItem = abi[0]
+  const signature = functionName
+
+  const data =
+    'inputs' in abiItem && abiItem.inputs
+      ? encodeAbiParameters(abiItem.inputs, args ?? [])
+      : undefined
+  return concatHex([signature, data ?? '0x'])
+}
