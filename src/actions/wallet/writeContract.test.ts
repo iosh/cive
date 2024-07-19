5fb72f7c49a9cbfe32afd9cafc214ec7ca828130
iosh <ioshx@foxmail.com>
2024-07-19 21:54:06 +0800

    chore: add writeContract and test

---
+++ src/actions/wallet/writeContract.test.ts
@@ -0,0 +1,51 @@
+import { parseUnits } from 'viem'
+import { afterAll, beforeAll, expect, test } from 'vitest'
+import { devConflux } from '~test/src/conflux/client.js'
+import { accounts, getTestAccount } from '~test/src/constants.js'
+import { Test20 } from '../../../test/src/contracts/Test20.js'
+import { deployTest20 } from '../../../test/src/utils.js'
+import { encodeFunctionData } from '../../utils/abi/encodeFunctionData.js'
+import { mine } from '../localNode/mine.js'
+import { sayHelloLocalNode } from '../localNode/sayHelloLocalNode.js'
+import { call } from '../public/call.js'
+import { writeContract } from './writeContract.js'
+
+const sourceAccount = getTestAccount(accounts[0])
+const client = devConflux.getClient({ account: sourceAccount })
+beforeAll(async () => {
+  await devConflux.start()
+  await sayHelloLocalNode(client)
+})
+
+afterAll(async () => {
+  await devConflux.stop()
+})
+
+test('default', async () => {
+  const { contractCreated: Test20Address } = await deployTest20(client)
+  expect(
+    await writeContract(client, {
+      address: Test20Address!,
+      abi: Test20.abi,
+      functionName: 'mint',
+      args: [sourceAccount.address, parseUnits('10', 18)],
+    }),
+  ).toBeDefined()
+
+  await mine(client)
+
+  expect(
+    await call(client, {
+      to: Test20Address!,
+      data: encodeFunctionData({
+        abi: Test20.abi,
+        functionName: 'balanceOf',
+        args: [sourceAccount.address],
+      }),
+    }),
+  ).toMatchInlineSnapshot(`
+    {
+      "data": "0x0000000000000000000000000000000000000000000000008ac7230489e80000",
+    }
+  `)
+})
---
+++ src/actions/wallet/writeContract.ts
@@ -0,0 +1,123 @@
+import type { Abi } from '../../types/abitype.js'
+
+import type { Transport } from 'viem'
+import type { Account } from '../../accounts/types.js'
+import type { Client } from '../../clients/createClient.js'
+import type { ErrorType } from '../../errors/utils.js'
+import type { GetAccountParameter } from '../../types/account.js'
+import type {
+  Chain,
+  DeriveChain,
+  GetChainParameter,
+} from '../../types/chain.js'
+import type {
+  ContractFunctionArgs,
+  ContractFunctionName,
+  ContractFunctionParameters,
+  GetValue,
+} from '../../types/contract.js'
+import type { Hex } from '../../types/misc.js'
+import type { Prettify, UnionEvaluate, UnionOmit } from '../../types/utils.js'
+import {
+  type EncodeFunctionDataErrorType,
+  type EncodeFunctionDataParameters,
+  encodeFunctionData,
+} from '../../utils/abi/encodeFunctionData.js'
+import type { FormattedTransactionRequest } from '../../utils/formatters/transactionRequest.js'
+import { getAction } from '../../utils/getAction.js'
+import {
+  type SendTransactionErrorType,
+  type SendTransactionReturnType,
+  sendTransaction,
+} from './sendTransaction.js'
+
+export type WriteContractParameters<
+  abi extends Abi | readonly unknown[] = Abi,
+  functionName extends ContractFunctionName<
+    abi,
+    'nonpayable' | 'payable'
+  > = ContractFunctionName<abi, 'nonpayable' | 'payable'>,
+  args extends ContractFunctionArgs<
+    abi,
+    'nonpayable' | 'payable',
+    functionName
+  > = ContractFunctionArgs<abi, 'nonpayable' | 'payable', functionName>,
+  chain extends Chain | undefined = Chain | undefined,
+  account extends Account | undefined = Account | undefined,
+  chainOverride extends Chain | undefined = Chain | undefined,
+  ///
+  allFunctionNames = ContractFunctionName<abi, 'nonpayable' | 'payable'>,
+  derivedChain extends Chain | undefined = DeriveChain<chain, chainOverride>,
+> = ContractFunctionParameters<
+  abi,
+  'nonpayable' | 'payable',
+  functionName,
+  args,
+  false,
+  allFunctionNames
+> &
+  GetChainParameter<chain, chainOverride> &
+  Prettify<
+    GetAccountParameter<account> &
+      GetValue<
+        abi,
+        functionName,
+        FormattedTransactionRequest<derivedChain>['value']
+      > & {
+        /** Data to append to the end of the calldata. Useful for adding a ["domain" tag](https://opensea.notion.site/opensea/Seaport-Order-Attributions-ec2d69bf455041a5baa490941aad307f). */
+        dataSuffix?: Hex | undefined
+      }
+  > &
+  UnionEvaluate<
+    UnionOmit<
+      FormattedTransactionRequest<derivedChain>,
+      'data' | 'from' | 'to' | 'value'
+    >
+  >
+
+export type WriteContractReturnType = SendTransactionReturnType
+
+export type WriteContractErrorType =
+  | EncodeFunctionDataErrorType
+  | SendTransactionErrorType
+  | ErrorType
+
+export async function writeContract<
+  chain extends Chain | undefined,
+  account extends Account | undefined,
+  const abi extends Abi | readonly unknown[],
+  functionName extends ContractFunctionName<abi, 'nonpayable' | 'payable'>,
+  args extends ContractFunctionArgs<
+    abi,
+    'nonpayable' | 'payable',
+    functionName
+  >,
+  chainOverride extends Chain | undefined,
+>(
+  client: Client<Transport, chain, account>,
+  parameters: WriteContractParameters<
+    abi,
+    functionName,
+    args,
+    chain,
+    account,
+    chainOverride
+  >,
+): Promise<WriteContractReturnType> {
+  const { abi, address, args, dataSuffix, functionName, ...request } =
+    parameters as WriteContractParameters
+  const data = encodeFunctionData({
+    abi,
+    args,
+    functionName,
+  } as EncodeFunctionDataParameters)
+  return getAction(
+    client,
+    sendTransaction,
+    'sendTransaction',
+  )({
+    data: `${data}${dataSuffix ? dataSuffix.replace('0x', '') : ''}`,
+    to: address,
+    ...request,
+  })
+}
