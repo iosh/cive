8cee22cce9e6a9ee292721e7221a7f9a160c9a87
iosh <ioshx@foxmail.com>
2024-07-20 12:29:02 +0800

    chore: add createContractEventFilter and test

---
+++ src/actions/public/createContractEventFilter.test.ts
@@ -0,0 +1,129 @@
+import { afterAll, beforeAll, expect, test } from 'vitest'
+import { devConflux } from '~test/src/conflux/client.js'
+import { accounts, getTestAccount } from '../../../test/src/constants.js'
+import { Test20 } from '../../../test/src/contracts/Test20.js'
+import { deployTest20 } from '../../../test/src/utils.js'
+import type { Address } from '../../accounts/types.js'
+import { sayHelloLocalNode } from '../localNode/sayHelloLocalNode.js'
+import { createContractEventFilter } from './createContractEventFilter.js'
+
+const sourceAccount = getTestAccount(accounts[0])
+const client = devConflux.getClient({ account: sourceAccount })
+let Test20Address: Address
+beforeAll(async () => {
+  await devConflux.start()
+  await sayHelloLocalNode(client)
+  const { contractCreated } = await deployTest20(client)
+  Test20Address = contractCreated!
+})
+
+afterAll(async () => {
+  await devConflux.stop()
+})
+
+test('args: address', async () => {
+  const filter = await createContractEventFilter(client, {
+    address: Test20Address,
+    abi: Test20.abi,
+  })
+  expect(filter.id).toBeDefined()
+})
+
+test('args: args', async () => {
+  const filter = await createContractEventFilter(client, {
+    address: Test20Address,
+    abi: Test20.abi,
+    eventName: 'Transfer',
+    args: {
+      from: sourceAccount.address,
+      to: sourceAccount.address,
+    },
+  })
+  expect(filter.abi).toEqual(Test20.abi)
+  expect(filter.args).toEqual({
+    from: sourceAccount.address,
+    to: sourceAccount.address,
+  })
+  expect(filter.eventName).toEqual('Transfer')
+
+  const filter2 = await createContractEventFilter(client, {
+    address: Test20Address,
+    abi: Test20.abi,
+    eventName: 'Transfer',
+    args: {
+      from: sourceAccount.address,
+    },
+  })
+
+  expect(filter.abi).toEqual(Test20.abi)
+  expect(filter2.args).toEqual({
+    from: sourceAccount.address,
+  })
+  expect(filter.eventName).toEqual('Transfer')
+
+  const filter3 = await createContractEventFilter(client, {
+    address: Test20Address,
+    abi: Test20.abi,
+    eventName: 'Transfer',
+    args: {
+      to: [accounts[0].base32Address, accounts[1].base32Address],
+    },
+  })
+  expect(filter.abi).toEqual(Test20.abi)
+  expect(filter3.args).toEqual({
+    to: [accounts[0].base32Address, accounts[1].base32Address],
+  })
+  expect(filter3.eventName).toEqual('Transfer')
+})
+
+test('args: epoch', async () => {
+  expect(
+    (
+      await createContractEventFilter(client, {
+        address: Test20Address,
+        abi: Test20.abi,
+        eventName: 'Transfer',
+        fromEpoch: 'latest_state',
+        toEpoch: 'latest_state',
+      })
+    ).id,
+  ).toBeDefined()
+
+  expect(
+    (
+      await createContractEventFilter(client, {
+        address: Test20Address,
+        abi: Test20.abi,
+        eventName: 'Transfer',
+        fromEpoch: 1n,
+        toEpoch: 'latest_state',
+      })
+    ).id,
+  ).toBeDefined()
+
+  expect(
+    (
+      await createContractEventFilter(client, {
+        address: Test20Address,
+        abi: Test20.abi,
+        eventName: 'Transfer',
+        fromEpoch: 'latest_state',
+        toEpoch: 1n,
+      })
+    ).id,
+  ).toBeDefined()
+})
+
+test('args: block number', async () => {
+  expect(
+    (
+      await createContractEventFilter(client, {
+        address: Test20Address,
+        abi: Test20.abi,
+        eventName: 'Transfer',
+        toBlock: 1n,
+        fromBlock: 1n,
+      })
+    ).id,
+  ).toBeDefined()
+})
---
+++ src/actions/public/createContractEventFilter.ts
@@ -0,0 +1,213 @@
+import type { Abi, Address } from '../../types/abitype.js'
+
+import type { Client } from '../../clients/createClient.js'
+
+import type { ErrorType } from '../../errors/utils.js'
+
+import type { Chain } from '../../types/chain.js'
+import type {
+  ContractEventName,
+  MaybeExtractEventArgsFromAbi,
+} from '../../types/contract.js'
+import type { Filter } from '../../types/filter.js'
+import type { Hex } from '../../types/misc.js'
+import {
+  type EncodeEventTopicsErrorType,
+  type EncodeEventTopicsParameters,
+  encodeEventTopics,
+} from '../../utils/abi/encodeEventTopics.js'
+
+import { type Transport, numberToHex } from 'viem'
+import type { NumberToHexErrorType, RequestErrorType } from 'viem/utils'
+import type { EpochNumber, EpochTag } from '../../types/block.js'
+import { createFilterRequestScope } from '../../utils/filters/createFilterRequestScope.js'
+
+export type CreateContractEventFilterParameters<
+  abi extends Abi | readonly unknown[] = Abi,
+  eventName extends ContractEventName<abi> | undefined = undefined,
+  args extends
+    | MaybeExtractEventArgsFromAbi<abi, eventName>
+    | undefined = undefined,
+  strict extends boolean | undefined = undefined,
+> = {
+  address?: Address | Address[] | undefined
+  abi: abi
+  eventName?: eventName | ContractEventName<abi> | undefined
+
+  /**
+   * Whether or not the logs must match the indexed/non-indexed arguments in the event ABI item.
+   * @default false
+   */
+  strict?: strict | boolean | undefined
+} & (undefined extends eventName
+  ? {
+      args?: undefined
+    }
+  : MaybeExtractEventArgsFromAbi<abi, eventName> extends infer eventFilterArgs
+    ? {
+        args?:
+          | eventFilterArgs
+          | (args extends eventFilterArgs ? args : never)
+          | undefined
+      }
+    : {
+        args?: undefined
+      }) &
+  (
+    | {
+        /**
+         * @default latest_checkpoint
+         */
+        fromEpoch?: EpochNumber | EpochTag
+        /**
+         * @default latest_checkpoint
+         */
+        toEpoch?: EpochNumber | EpochTag
+        /**
+         * @default null
+         */
+        toBlock?: never | undefined
+        /**
+         * @default null
+         */
+        fromBlock?: never | undefined
+
+        blockHashes?: never | undefined
+      }
+    | {
+        /**
+         * @default latest_checkpoint
+         */
+        fromEpoch?: never | undefined
+        /**
+         * @default latest_checkpoint
+         */
+        toEpoch?: never | undefined
+        /**
+         * @default null
+         */
+        toBlock: EpochNumber
+        /**
+         * @default null
+         */
+        fromBlock: EpochNumber
+
+        blockHashes?: never | undefined
+      }
+    | {
+        /**
+         * @default latest_checkpoint
+         */
+        fromEpoch?: never | undefined
+        /**
+         * @default latest_checkpoint
+         */
+        toEpoch?: never | undefined
+        /**
+         * @default null
+         */
+        toBlock?: never | undefined
+        /**
+         * @default null
+         */
+        fromBlock?: never | undefined
+
+        blockHashes: Hex[]
+      }
+  )
+
+export type CreateContractEventFilterReturnType<
+  abi extends Abi | readonly unknown[] = Abi,
+  eventName extends ContractEventName<abi> | undefined = undefined,
+  args extends
+    | MaybeExtractEventArgsFromAbi<abi, eventName>
+    | undefined = undefined,
+  strict extends boolean | undefined = undefined,
+  fromBlock extends EpochNumber | EpochTag | undefined = undefined,
+  toBlock extends EpochNumber | EpochTag | undefined = undefined,
+> = Filter<'event', abi, eventName, args, strict, fromBlock, toBlock>
+
+export type CreateContractEventFilterErrorType =
+  | EncodeEventTopicsErrorType
+  | RequestErrorType
+  | NumberToHexErrorType
+  | ErrorType
+
+export async function createContractEventFilter<
+  chain extends Chain | undefined,
+  const abi extends Abi | readonly unknown[],
+  eventName extends ContractEventName<abi> | undefined,
+  args extends MaybeExtractEventArgsFromAbi<abi, eventName> | undefined,
+  strict extends boolean | undefined = undefined,
+>(
+  client: Client<Transport, chain>,
+  parameters: CreateContractEventFilterParameters<abi, eventName, args, strict>,
+): Promise<CreateContractEventFilterReturnType<abi, eventName, args, strict>> {
+  const {
+    address,
+    abi,
+    args,
+    eventName,
+    fromBlock,
+    strict,
+    toBlock,
+    blockHashes,
+    fromEpoch = 'latest_checkpoint',
+    toEpoch = 'latest_checkpoint',
+  } = parameters as CreateContractEventFilterParameters
+
+  const _fromEpoch = fromBlock ? undefined : fromEpoch
+  const _toEpoch = toBlock ? undefined : toEpoch
+  const getRequest = createFilterRequestScope(client, {
+    method: 'cfx_newFilter',
+  })
+
+  const topics = eventName
+    ? encodeEventTopics({
+        abi,
+        args,
+        eventName,
+      } as unknown as EncodeEventTopicsParameters)
+    : undefined
+
+  const params = blockHashes
+    ? {
+        fromBlock:
+          typeof fromBlock === 'bigint' ? numberToHex(fromBlock) : fromBlock,
+        toBlock: typeof toBlock === 'bigint' ? numberToHex(toBlock) : toBlock,
+        blockHashes,
+        address,
+        topics,
+      }
+    : {
+        fromEpoch:
+          typeof _fromEpoch === 'bigint' ? numberToHex(_fromEpoch) : _fromEpoch,
+        toEpoch:
+          typeof _toEpoch === 'bigint' ? numberToHex(_toEpoch) : _toEpoch,
+        fromBlock:
+          typeof fromBlock === 'bigint' ? numberToHex(fromBlock) : fromBlock,
+        toBlock: typeof toBlock === 'bigint' ? numberToHex(toBlock) : toBlock,
+        address,
+        topics,
+      }
+
+  const id: Hex = await client.request({
+    method: 'cfx_newFilter',
+    params: [params],
+  })
+
+  return {
+    abi,
+    args,
+    eventName,
+    id,
+    request: getRequest(id),
+    strict: Boolean(strict),
+    type: 'event',
+  } as unknown as CreateContractEventFilterReturnType<
+    abi,
+    eventName,
+    args,
+    strict
+  >
+}
