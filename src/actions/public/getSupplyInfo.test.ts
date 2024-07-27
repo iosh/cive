9cdd19b3cb6b63a76a137555300671a7982c099d
iosh <ioshx@foxmail.com>
2024-07-27 15:14:57 +0800

    chore: add getSupplyInfo test

---
+++ src/actions/public/getSupplyInfo.test.ts
@@ -0,0 +1,30 @@
+import { afterAll, beforeAll, expect, test } from 'vitest'
+import { devConflux } from '~test/src/conflux/client.js'
+import { accounts, getTestAccount } from '../../../test/src/constants.js'
+
+import { sayHelloLocalNode } from '../localNode/sayHelloLocalNode.js'
+import { getSupplyInfo } from './getSupplyInfo.js'
+
+const client = devConflux.getClient({
+  account: getTestAccount(accounts[0]),
+})
+beforeAll(async () => {
+  await devConflux.start()
+  await sayHelloLocalNode(client)
+})
+
+afterAll(async () => {
+  await devConflux.stop()
+})
+
+test('default', async () => {
+  expect(await getSupplyInfo(client)).toMatchInlineSnapshot(`
+    {
+      "totalCirculating": 100050000000000000000000n,
+      "totalCollateral": 50062500000000000000n,
+      "totalEspaceTokens": 0n,
+      "totalIssued": 5000100000000000000000000000n,
+      "totalStaking": 2000000000000000000000000n,
+    }
+  `)
+})
