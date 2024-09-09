e72ddcf0891357ceb356132bbd87eca08315cf6f
iosh <ioshx@foxmail.com>
2024-09-09 20:46:02 +0800

    chore: add packages to docs

---
+++ site/tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./pages/docs/**/*.{md,mdx}', "./pages/index.mdx"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
