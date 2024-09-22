7c99334f43f91ecfb5d93e62084a8705d02cbb1a
iosh <ioshx@foxmail.com>
2024-09-22 14:07:56 +0800

    chore: update example

--- examples/client_public_client/README.md
+++ examples/client_public_client/README.md
@@ -1,3 +1,3 @@
-# Public Client Example Example
+# Public Client Example
 
 [![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github.com/iosh/cive/tree/main/examples/client_public_client)
--- examples/client_public_client/index.tsx
+++ examples/client_public_client/index.tsx
@@ -2,55 +2,6 @@ import React from 'react'
 import App from './src/App'
 import 'bulma/css/bulma.css'
 
-// /**
-//  * Websocket
-//  */
-
-// const webSocketClient = createPublicClient({
-//   chain: testnet,
-//   transport: webSocket('wss://test.confluxrpc.org/ws'),
-// })
-
-// async function wsGetEpochNumber() {
-//   await render('#wsGetEpochNumber', '#wsEpochNumber', async () => {
-//     // you can set the epochTag , if you don't want to use cache you can set cacheTime to 0, the default cacheTime is `client.cacheTime`
-//     return await webSocketClient.getEpochNumber({
-//       epochTag: 'latest_mined',
-//       cacheTime: 0,
-//     })
-//   })
-// }
-// const wsEpochNumberButton =
-//   document.querySelector<HTMLButtonElement>('#wsGetEpochNumber')!
-// wsEpochNumberButton.addEventListener('click', wsGetEpochNumber)
-
-// /**
-//  * Fallback
-//  */
-
-// const fallbackClient = createPublicClient({
-//   chain: testnet,
-//   transport: fallback([
-//     // The client will try to use http first and fallback to next.
-//     http('http://fake.example.com'),
-//     http('https://test.confluxrpc.com'),
-//   ]),
-// })
-
-// async function fallbackGetEpochNumber() {
-//   await render('#fallbackGetEpochNumber', '#fallbackEpochNumber', async () => {
-//     // you can set the epochTag , if you don't want to use cache you can set cacheTime to 0, the default cacheTime is `client.cacheTime`
-//     return await fallbackClient.getEpochNumber({
-//       epochTag: 'latest_mined',
-//       cacheTime: 0,
-//     })
-//   })
-// }
-// const fallbackEpochNumberButton = document.querySelector<HTMLButtonElement>(
-//   '#fallbackGetEpochNumber',
-// )!
-// fallbackEpochNumberButton.addEventListener('click', fallbackGetEpochNumber)
-
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
