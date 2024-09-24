91bcadaad848f33d0658017b77aa3e2ea43f318c
iosh <ioshx@foxmail.com>
2024-09-24 20:24:56 +0800

    chore: format

--- examples/epoch_watch_epoch_number/src/App.tsx
+++ examples/epoch_watch_epoch_number/src/App.tsx
@@ -1,27 +1,27 @@
-import { http, createPublicClient } from "cive";
-import { mainnet } from "cive/chains";
-import { useCallback, useEffect, useState } from "react";
+import { http, createPublicClient } from 'cive'
+import { mainnet } from 'cive/chains'
+import { useEffect, useState } from 'react'
 
 const client = createPublicClient({
   chain: mainnet,
   transport: http(),
-});
+})
 export default function App() {
-  const [epochNumber, setEpochNumber] = useState("");
+  const [epochNumber, setEpochNumber] = useState('')
 
   useEffect(() => {
     const unwatch = client.watchEpochNumber({
       onEpochNumber: (epochNumber) => {
-        setEpochNumber(`${epochNumber}`);
+        setEpochNumber(`${epochNumber}`)
       },
       pollingInterval: 1000,
-    });
-    return unwatch;
-  }, []);
+    })
+    return unwatch
+  }, [])
 
   return (
     <div className="block">
       <span className="is-size-5">Epoch number: {epochNumber}</span>
     </div>
-  );
+  )
 }
