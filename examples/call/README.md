a54030290fae73a4b35640146d44c46caefab1d0
iosh <ioshx@foxmail.com>
2024-10-08 20:32:02 +0800

    chore: add call example

---
+++ examples/call/README.md
@@ -0,0 +1,3 @@
+# Call Example
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github.com/iosh/cive/tree/main/examples/call)
---
+++ examples/call/src/App.tsx
@@ -0,0 +1,51 @@
+import {
+  http,
+  createPublicClient,
+  encodeFunctionData,
+  decodeFunctionResult,
+} from 'cive'
+import { testnet } from 'cive/chains'
+import { useCallback, useState } from 'react'
+import contract from './contract'
+
+const CONTRACT_ADDRESS = 'cfxtest:acfrcwu7yn4ysjybux326my6a743zw2zwjps5had1g'
+const client = createPublicClient({
+  chain: testnet,
+  transport: http(),
+})
+export default function App() {
+  const [tokenName, setTokenName] = useState('')
+
+  const handleGetTokenName = useCallback(async () => {
+    const { data } = await client.call({
+      to: CONTRACT_ADDRESS,
+      data: encodeFunctionData({
+        abi: contract.abi,
+        functionName: 'name',
+      }),
+    })
+    setTokenName(
+      data
+        ? decodeFunctionResult({
+            abi: contract.abi,
+            functionName: 'name',
+            data,
+            networkId: testnet.id,
+          })
+        : '',
+    )
+  }, [])
+
+  return (
+    <div className="box">
+      <div className="columns is-3 ">
+        <div className="column">
+          <button className="button" onClick={handleGetTokenName}>
+            Get token name by call
+          </button>
+          <span className="is-size-5">{tokenName}</span>
+        </div>
+      </div>
+    </div>
+  )
+}
