eb951d036fe3f0f363531689f707197f52c14ede
iosh <ioshx@foxmail.com>
2025-06-02 13:02:01 +0800

    chore: update

--- src/utils/signature/toPrefixedMessage.test.ts
+++ src/utils/signature/toPrefixedMessage.test.ts
@@ -1,19 +1,10 @@
 import { expect, test } from 'vitest'
 
-import { PersonalMessage } from 'js-conflux-sdk'
 import { stringToHex } from 'viem'
 import { toPrefixedMessage } from './toPrefixedMessage.js'
 
 test('default', () => {
   const hello = 'hello world'
-  expect(toPrefixedMessage(hello)).toBe(
-    stringToHex(PersonalMessage.personalMessage(hello)),
-  )
-
-  expect(toPrefixedMessage({ raw: stringToHex('hello world') })).toBe(
-    stringToHex(PersonalMessage.personalMessage(hello)),
-  )
-
   expect(toPrefixedMessage(hello)).toMatchInlineSnapshot(
     `"0x19436f6e666c7578205369676e6564204d6573736167653a0a313168656c6c6f20776f726c64"`,
   )
